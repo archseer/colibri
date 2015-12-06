171fb9b994882438727973b9e28d2624b039c03a
Blaž Hrastnik <blaz.hrast@gmail.com>
2015-12-06 20:14:42 +0100

    Add album view.

--- app/models/track.js
+++ app/models/track.js
@@ -2,8 +2,8 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   title: DS.attr('string'),
-  duration: DS.attr('integer'),
-  disc: DS.attr('integer'),
+  duration: DS.attr('number'),
+  disc: DS.attr('number'),
   filename: DS.attr('string'),
 
   album: DS.belongsTo('album'),
--- app/router.js
+++ app/router.js
@@ -7,6 +7,7 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('queue');
+  this.route('album', { path: '/albums/:album_id'});
 });
 
 export default Router;
