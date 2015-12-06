ebf0302c652a59ac01a5616e1d0e3b242ceced38
Blaž Hrastnik <blaz.hrast@gmail.com>
2015-12-06 18:49:05 +0100

    Serve the cover art.
    A bit hacky, since the URL is relative...

--- app/components/album-cover.js
+++ app/components/album-cover.js
@@ -2,6 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   classNames: ['cover'],
+
+  cover: function() {
+    return `http://localhost:4000/${this.get('album.cover')}`
+  }.property('album.cover'),
+
   actions: {
     playTrack: function() {
       var audio = this.get('audiojs');
--- app/models/album.js
+++ app/models/album.js
@@ -2,6 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   title: DS.attr('string'),
+  cover: DS.attr('string'),
   artist: DS.belongsTo('artist'),
   tracks: DS.hasMany('track')
 });
