1407fd10c7eb55a1dda8d42762f2698337c925e5
Blaž Hrastnik <blaz.hrast@gmail.com>
2015-12-13 23:24:05 +0100

    Add a header with the tracklist total stats (count, total duration).

--- app/components/track-list.js
+++ app/components/track-list.js
@@ -4,4 +4,7 @@ export default Ember.Component.extend({
   tagName: 'ul',
   classNames: ['tracklist'],
 
+  totalDuration: function() {
+    return this.get('tracks').mapBy('duration').reduce(function(a, b) {return a + b}, 0);
+  }.property('tracks.@each.duration')
 });
